1d908a4ea2f961f9ffb5c2e0b65f5357e6efee63
Dan Apczynski <dapczynski@zendesk.com>
2015-02-28 21:53:31 -0800

    wip

---
+++ Tweetier Than Thou/ProfileCell.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileCell.swift
+//  Tweetier Than Thou
+//
+//  Created by Daniel Apczynski on 2/28/15.
+//  Copyright (c) 2015 Dan Apczynski. All rights reserved.
+//
+
+import UIKit
+
+class ProfileCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Tweetier Than Thou/ProfileViewController.swift
+++ Tweetier Than Thou/ProfileViewController.swift
@@ -8,17 +8,49 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        tableView.datasource = self
+        tableView.delegate = self
         // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        var cell : UITableViewCell?
+        
+        switch indexPath.row {
+        case 1 : cell = dequeueProfileCell() as ProfileCell!
+        default : cell = dequeueTweetCell(indexPath.row) as TweetCell!
+        }
+        
+        return cell!
+    }
+    
+    func dequeueProfileCell() -> ProfileCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier("ProfileCell") as ProfileCell!
+        
+        return cell
+    }
+    
+    func dequeueTweetCell(index: Int) -> TweetCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier("TweetCell") as TweetCell!
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
     }
     
 
